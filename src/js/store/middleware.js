2aad4e7fbf332add9b703a324057ddf867f961d1
mac-s-g <mac.gainor@gmail.com>
2019-04-06 03:53:41 -0700

    updated build

--- src/js/store/middleware.js
+++
@@ -1,7 +0,0 @@
-import thunkMiddleware from 'redux-thunk'
-import { createBrowserHistory } from 'history'
-import { routerMiddleware } from 'connected-react-router'
-
-export const history = createBrowserHistory()
-
-export default [thunkMiddleware, routerMiddleware(history)]
