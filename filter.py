6918e8e7432daf021463cd7ad4df9119784cfbbe
Parvez Khan <parvezkhan0802@gmail.com>
2023-10-31 20:00:38 +0530

    added filter by popularity

---
+++ filter.py
@@ -0,0 +1,35 @@
+import requests
+
+# Set your GitHub username and personal access token
+username = "YourUsername"
+token = "YourPersonalAccessToken"
+
+# Set the owner and repository
+owner = "RepositoryOwner"
+repository = "RepositoryName"
+
+# Define the API endpoint for fetching issues
+url = f"https://api.github.com/repos/{owner}/{repository}/issues"
+
+# Define the parameters to sort by reactions
+params = {
+    "state": "open",  # You can change this to "closed" if you want closed issues
+    "sort": "reactions",  # Sort by reactions
+    "direction": "desc",  # Sort in descending order (most reactions first)
+}
+
+# Set up authentication using your personal access token
+headers = {
+    "Authorization": f"token {token}"
+}
+
+# Make the GET request to fetch the sorted issues
+response = requests.get(url, params=params, headers=headers)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    issues = response.json()
+    for issue in issues:
+        print(f"Issue #{issue['number']}: {issue['title']} ({issue['reactions']['total']} reactions)")
+else:
+    print(f"Error: Unable to fetch issues. Status code {response.status_code}")
